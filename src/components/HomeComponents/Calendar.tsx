4cc9aa75a34860ae8c79a628629e9f63f4677e74
fMart8421 <fabio.m@ua.pt>
2021-05-25 17:54:44 +0100

    added app.css, changed buttons to calendar and added header icon no proper css, mobile responsive

--- src/components/HomeComponents/Calendar.tsx
+++ src/components/HomeComponents/Calendar.tsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import {CalendarComponent, ChangedEventArgs, RenderDayCellEventArgs} from '@syncfusion/ej2-react-calendars'
 import {Row, Col} from 'react-bootstrap';
 import './Calendar.css'
+import { IonButton, IonLabel, IonText } from '@ionic/react';
 const Calendar = () => {
     const [date, setDate] = useState(new Date());
     const dateValues = [new Date("05/24/2021"), new Date("05/27/2021"), new Date("05/30/2021")];
@@ -24,8 +25,12 @@ const Calendar = () => {
     }
     return (
         <div className="home-calendar">
-        <Row className="calendar-header d-flex justify-content-center align-items-center mx-0">
-                            <div><h5><strong className="primary">O seu Calendário</strong></h5></div>
+        <Row className="mx-0">
+                            <div className="calendar-header d-flex flex-row justify-content-between mx-0" style={{width:"100%"}} > 
+                                <IonText className="d-flex align-items-center col-sm-6" color="tertiary">
+                                <h4 style={{fontWeight:"bold"}} id="your-calendar" className="my-0 ml-2">O seu Calendário</h4>
+                                </IonText>
+                                <IonButton size="small" className="col-sm-4 col-lg-5"><IonLabel>Adicionar Lembrete</IonLabel></IonButton></div>
                         </Row>
             <CalendarComponent
                     id="calendar"
@@ -36,10 +41,12 @@ const Calendar = () => {
                     onChange={onClickDay}
                     min={minDates[0]}
                     max={maxDates[0]}
+                    showTodayButton={false}
                     ></CalendarComponent>
         </div>
     )
 }
 
 export default Calendar
+
 
