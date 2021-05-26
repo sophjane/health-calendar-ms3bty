3f42f491880b43d06eb814c2d970d0b79d3db5ef
Patrícia Dias <pmd8@ua.pt>
2021-05-26 12:49:00 +0100

    Revert "Dev/settings updated"

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,34 +1,41 @@
 import { IonContent, 
   IonHeader, 
-  IonPage 
+  IonPage, 
+  IonTitle,
+  IonToolbar 
 } from '@ionic/react';
 import PersonalInfo from '../components/Definicoes/PersonalInfo';
 import Buttons from '../components/Definicoes/Buttons';
 import Reminder from '../components/Definicoes/Reminder';
-
-import Toolbar from "../components/Toolbar";
+import './Settings.css';
 
 const userDetails: React.FC = () => {
-  /* const onSave = (nameP: string, email: string, pacientNumber: string) => {
+  const onSave = (nameP: string, email: string, pacientNumber: string) => {
     personDetails.name = nameP;
     personDetails.email = email;
     personDetails.pacientNumber = pacientNumber;
     console.log(personDetails.name);
-  } */
+  }
   const personDetails = {
-/*     onSave : onSave, */
+    onSave : onSave,
     name : 'Joana Silva',
     email : 'joanasilva@gmail.com',
     pacientNumber : '123456789'
   }
   return (
-    <IonPage>
+    <IonPage className = 'Page'>
       <IonHeader>
-        <Toolbar title="Definições"></Toolbar>
+        <IonToolbar title="Definições">
+        <IonTitle>Definições</IonTitle>
+        </IonToolbar>
       </IonHeader>
       <IonContent>
         <PersonalInfo { ...personDetails} />
+      </IonContent>
+      <IonContent>
         <Buttons />
+      </IonContent>
+      <IonContent style={{overflow: "hidden"}}>
         <Reminder />
       </IonContent>
     </IonPage>
