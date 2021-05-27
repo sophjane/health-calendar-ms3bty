bf57cceb4e513b8d947b7555e94e814fee63a011
fMart8421 <fabio.m@ua.pt>
2021-05-27 02:12:15 +0100

    removed homecomponent

--- src/components/HomeComponents/Calendar.tsx
+++ src/components/HomeComponents/Calendar.tsx
@@ -5,20 +5,27 @@ import './Calendar.css'
 import { IonButton, IonLabel, IonText } from '@ionic/react';
 const Calendar = () => {
     const [date, setDate] = useState(new Date());
-    const dateValues = [new Date("05/24/2021"), new Date("05/27/2021"), new Date("05/30/2021")];
+    const dateAnalysis = [new Date("06/10/2021"), new Date("05/27/2021"), new Date("06/12/2021")];
+    const dateAppointments = [new Date("06/02/2021"), new Date("06/01/2021"), new Date("05/30/2021")];
     const maxDates = [new Date("06/30/2021")]
-    const minDates = [new Date()]
+    const minDates = [new Date()];
+    console.log((new Date()).getHours() - (new Date("06/30/2021")).getHours())
     var currentDay;
     const onClickDay = (args: ChangedEventArgs): void =>{
         currentDay = args.value?.toLocaleDateString();
         console.log(currentDay);   
     }
     const highlightWeekend = (args?: RenderDayCellEventArgs): void =>{
-        dateValues.forEach(element => {
+        dateAnalysis.forEach(element => {
             if ((args?.date as Date).toLocaleDateString() === element.toLocaleDateString()) {
                 // To highlight the week end of every month
-                args?.element?.classList.add('available-day');
-                console.log((args?.date as Date).toLocaleDateString())
+                args?.element?.classList.add('analysis');
+            }
+        });
+        dateAppointments.forEach(element => {
+            if ((args?.date as Date).toLocaleDateString() === element.toLocaleDateString()) {
+                // To highlight the week end of every month
+                args?.element?.classList.add('appointment');
             }
         });
         
@@ -27,7 +34,7 @@ const Calendar = () => {
         <div className="home-calendar">
         <Row className="mx-0">
                             <div className="calendar-header d-flex flex-row justify-content-between mx-0" style={{width:"100%"}} > 
-                                <IonText className="d-flex align-items-center col-sm-6" color="tertiary">
+                                <IonText className="d-flex align-items-center col-sm-6" color="light">
                                 <h4 style={{fontWeight:"bold"}} id="your-calendar" className="my-0 ml-2">O seu Calendário</h4>
                                 </IonText>
                                 <IonButton href="/settings" size="small" className="col-sm-4 col-lg-5"><IonLabel>Adicionar Lembrete</IonLabel></IonButton></div>
